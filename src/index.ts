fe60cf99b63e39d6bf2b0b5de0672a144622bb50
afeiship <1290657123@qq.com>
2025-05-23 10:41:40 +0800

    feat: add basic comments

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,50 @@
-// 1-9
-// quotient 商
-// remainder 余数
+type RunTimeEnv =
+  // 微信小程序
+  | 'WX_MINI_PROGRAM'
+  // 微信 Web
+  | 'WX_WEB'
+  // 微信开发工具
+  | 'WX_DEVTOOLS'
+  // 企业微信
+  | 'WX_WXWORK'
+  // 普通的Web
+  | 'WEB'
+  // alo7 IOS app 里的 WEB
+  | 'ALO7_APP_IOS'
+  // alo7 Android app 里的 WEB
+  | 'ALO7_APP_ANDROID'
+  // 其它未知情况
+  | 'UNKNOWN';
 
-const runtimeEnv = (inNumber: number | string): number[] => {
-  const str = String(inNumber);
-  const rdx = str.length - 1;
-  return str.split('').map((n: string, idx) => {
-    if (n === '0') return 0;
-    return parseInt(n, 10) * Math.pow(10, rdx - idx);
-  });
+declare const wx: any;
+const runtimeEnv = (): RunTimeEnv => {
+  // 判断是否运行在微信小程序中
+  if (typeof wx !== 'undefined' && wx.getSystemInfoSync) {
+    const systemInfo = wx.getSystemInfoSync();
+    if (systemInfo && systemInfo.platform === 'devtools') {
+      return 'WX_DEVTOOLS';
+    } else if (systemInfo && (systemInfo.platform === 'ios' || systemInfo.platform === 'android')) {
+      return 'WX_MINI_PROGRAM';
+    }
+  }
+
+  // 企业微信
+  if (typeof wx !== 'undefined' && wx.qy) {
+    return 'WX_WXWORK';
+  }
+
+  // 判断是否运行在微信 JSSDK 环境中
+  if (typeof wx !== 'undefined' && typeof wx.miniProgram !== 'undefined') {
+    return 'WX_WEB';
+  }
+
+  // 判断是否运行在普通的 Web 环境中
+  if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+    return 'WEB';
+  }
+
+  // 默认返回未知环境
+  return 'UNKNOWN';
 };
 
 export default runtimeEnv;
